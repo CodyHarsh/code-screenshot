7d4286ad6cc14f0ea5bff8693115fecdde2ca597
CodyHarsh <harshkailash89@gmail.com>
2023-08-31 20:10:36 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='dark bg-neutral-950 text-white text-lg 	text-center'>
+        Made with 💓 By Harsh: <a href="https://www.linkedin.com/in/codyharsh/" className='md:hover:underline text-blue-400	'>Linkedin</a>
+    </div>
+  )
+}
